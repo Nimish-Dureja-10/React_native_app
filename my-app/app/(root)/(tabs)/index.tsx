d0f831b56c41e91003d486e4531844a4f6ae33cd
Nimish-Dureja-10 <durejanimish14@gmail.com>
2024-12-28 18:08:51 +0530

    Assests, fonts and static data added and updated tailwind.config.js

--- my-app/app/(root)/(tabs)/index.tsx
+++ my-app/app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="text-xl font-bold my-8">Welcome to ReState</Text>
+      <Text className="text-3xl font-rubik my-10">Welcome to ReState</Text>
       <Link href={"/sign-in"} >Sign In</Link>
       <Link href={"/explore"} >Explore</Link>
       <Link href={"/profile"} >Profile</Link>
