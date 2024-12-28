54975a10762efdb81a540aef0045502b50ff93f0
Nimish-Dureja-10 <durejanimish14@gmail.com>
2024-12-28 15:49:10 +0530

    App and Native wind setup

---
+++ my-app/app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Explore = () => {
+  return (
+    <View>
+      <Text className='text-xl font-bold'>explore</Text>
+    </View>
+  )
+}
+
+export default Explore
